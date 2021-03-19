4d77aca36c11315cf53e57214f2a783fb55b2ca7
NikanorUrlaku <80978731+NikanorUrlaku@users.noreply.github.com>
2021-03-19 20:54:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", checkScroll);
+
+document.addEventListener("DOMContentLoaded",checkScroll);
+
+function checkScroll() {
+    let scrollPoz = window.scrollY;
+    if(scrollPoz > 0){
+        header.classList.add('red')
+    } else {
+        header.classList.remove('red')
+    }
+};
